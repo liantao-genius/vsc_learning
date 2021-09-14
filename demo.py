2b05d5ff9e3e9edc78d69d826508011d93360129
liantao <liantao@edu.bme.hu>
2021-09-14 23:43:47 +0800

    first code

---
+++ demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+# -*- coding: utf-8 -*-
+# version: v0.0.1
+# author: lian tao
+# contact: liantao@edu.bme.hu
+# project: vsc_learning
+# filename: demo.py
+# datetime: 2021-09-14 23:38
+# description: version control learning
+import numpy as np
+import matplotlib.pyplot as plt
+a=[1,2,3]
+print(a)
